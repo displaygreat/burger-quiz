1f67bfab30fd102c1d1881d80ce4932a8b034344
displaygreat <displaygreat@gmail.com>
2021-09-30 09:56:53 +0300

    change publicPath

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js",
+    publicPath: "/burger-quiz/",
   },
   // Customize the webpack build process
   plugins: [
